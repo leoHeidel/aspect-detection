7cb4cc22e797ce04693d26170515ce3b6584397e
unknown <nicolas.guerin@polytechnique.edu>
2021-02-27 14:00:14 +0100

    minor

--- run.py
+++ run.py
@@ -20,23 +20,33 @@ def import_from_path(path_to_module, obj_name = None):
     return obj
 
 if __name__ == "__main__":
+    
+    #GET THE ARGUMENTS
     parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument("-c", "--config", help = "File path to the config file")
+    parser.add_argument("-l", "--language", help = "Language on which we perform aspect extraction")
     parser.add_argument("-k", help="Number of aspects")
     parser.add_argument("-s", "--sentence", required=True)
     args = parser.parse_args()
 
+    #LOAD CONFIG FILE
     with open(args.config) as config_file:
         config = yaml.safe_load(config_file)
-            
+        
+    #LOAD LANGUAGE        
     w2v = src.datasets.read_english_w2v(lim=10000)
     train_texts, train_labels = src.datasets.read_imdb()
         
     sentence = config['sentences']['sentence%s'%args.sentence]
     
-    print(config["model"]["parameters"])
+    #LOAD DETECTOR
+    detector_params = config["model"]["parameters"]
+    
+    if args.k != None:
+        detector_params['k'] = args.k
+    
     detector = import_from_path(config["model"]["filepath"],
-                                config["model"]["class"])(w2v, **config["model"]["parameters"])
+                                config["model"]["class"])(w2v, **detector_params)
     
     #detector = kmeans.KMeansAspectDetector(w2v, k=args.k)
     res = detector.predict_sentence(sentence)
